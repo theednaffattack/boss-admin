3d475df3e2d918f01ba6f79755d2077fd6371216
Eddie Naff <eddie@eddienaff.dev>
2021-01-23 13:46:59 -0800

    Change function to check args

--- src/script.add-certificate.ts
+++ src/script.add-certificate.ts
@@ -1,8 +1,9 @@
 import { addCertificate } from "./add-certificate";
 
 async function runAddCertificateCommand() {
-    const secret = await addCertificate();
-    console.log("VIEW RETURNED secret", secret);
+    const [arg] = process.argv.slice(2);
+
+    return await addCertificate(arg);
 }
 
 runAddCertificateCommand();
