4ea8156459e802cdcc0df275fa2e5083baf224be
Eddie Naff <eddie@eddienaff.dev>
2021-01-23 10:19:15 -0800

    Rename to add certificate

--- src/script.add-certificate.ts
+++ src/script.add-certificate.ts
@@ -1,8 +1,8 @@
 import { addCertificate } from "./add-certificate";
 
-async function runUploadCommand() {
+async function runAddCertificateCommand() {
     const secret = await addCertificate();
     console.log("VIEW RETURNED secret", secret);
 }
 
-runUploadCommand();
+runAddCertificateCommand();
