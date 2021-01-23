dc87deff9608143e62f20e7f820f472840eeafae
Eddie Naff <eddie@eddienaff.dev>
2021-01-23 10:17:01 -0800

    Add jest for testing

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+    verbose: false,
+    preset: "ts-jest",
+    testEnvironment: "node",
+};
+export default config;
