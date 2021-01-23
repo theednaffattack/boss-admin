a8c17ced46532d8268a66624af21707289f08136
Eddie Naff <eddie@eddienaff.dev>
2021-01-23 13:45:29 -0800

    Add domain argument

--- src/add-certificate.ts
+++ src/add-certificate.ts
@@ -4,7 +4,14 @@ import path from "path";
 
 export type ACMConfigOptions = ACMClientConfig["credentials"];
 
-export async function addCertificate(): Promise<ImportCertificateResponse> {
+export async function addCertificate(domain: string): Promise<ImportCertificateResponse> {
+    if (!domain) {
+        throw Error(`The 'addCertificate' function expects a domain argument.`);
+    }
+    if (typeof domain !== "string") {
+        throw Error(`The expected argument 'domain' should be of type string.`);
+    }
+
     let credentials: ACMConfigOptions;
 
     if (process.env.SC_ADMIN_ACCESS_KEY_ID && process.env.SC_ADMIN_SECRET_ACCESS_KEY) {
@@ -20,11 +27,11 @@ export async function addCertificate(): Promise<ImportCertificateResponse> {
     });
 
     const importCommand = new ImportCertificateCommand({
-        Certificate: fs.readFileSync(path.resolve(__dirname, `../secret/cert.pem`)),
-        PrivateKey: fs.readFileSync(path.resolve(__dirname, `../secret/key.pem`)),
-        CertificateArn: "arn:aws:acm:us-east-1:942394920512:certificate/29d4eafd-1014-4d2a-a506-8466588fbde4",
-        CertificateChain: fs.readFileSync(path.resolve(__dirname, `../secret/fullchain.pem`)),
-        Tags: [{ Key: "operation", Value: "test" }],
+        Certificate: fs.readFileSync(path.resolve(__dirname, `../secret/${domain}/cert.pem`)),
+        PrivateKey: fs.readFileSync(path.resolve(__dirname, `../secret/${domain}/key.pem`)),
+        // CertificateArn: "arn:aws:acm:us-east-1:942394920512:certificate/29d4eafd-1014-4d2a-a506-8466588fbde4",
+        CertificateChain: fs.readFileSync(path.resolve(__dirname, `../secret/${domain}/fullchain.pem`)),
+        Tags: [{ Key: "secretsUpload", Value: "initial" }],
     });
 
     try {
