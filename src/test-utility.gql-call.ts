2536b497c9a69614e728a1986be2f25e5ba790f8
Eddie Naff <eddie@eddienaff.dev>
2021-02-02 14:18:24 -0800

    Add graphql test utility

---
+++ src/test-utility.gql-call.ts
@@ -0,0 +1,39 @@
+import { graphql, GraphQLSchema } from "graphql";
+import { Maybe } from "graphql/jsutils/Maybe";
+import "reflect-metadata";
+
+import { createSchema } from "./utility.create-schema";
+
+// prettier-ignore
+interface Options {
+  source: string;
+  variableValues?: Maybe<{
+    [key: string]: any;
+  }>;
+  userId?: string;
+}
+
+let schema: GraphQLSchema;
+
+export const gqlCall = async ({ source, variableValues, userId }: Options): Promise<any> => {
+    if (!schema) {
+        schema = await createSchema();
+    }
+
+    return await graphql({
+        schema,
+        source,
+        variableValues,
+        contextValue: {
+            req: {
+                session: {
+                    userId,
+                },
+            },
+            res: {
+                clearCookie: (): jest.Mock<any, any> => jest.fn(),
+            },
+            userId,
+        },
+    });
+};
